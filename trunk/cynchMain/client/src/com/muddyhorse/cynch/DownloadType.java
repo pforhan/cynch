06abc5c27618978be16fc380832a3f4b6b443c69
pforhan <pforhan@fd6e4df3-511c-0410-8f73-17a982673581>
2006-09-05 17:12:03 +0000

    Start of admin tool.

--- trunk/cynchMain/client/src/com/muddyhorse/cynch/DownloadType.java
+++ trunk/cynchMain/client/src/com/muddyhorse/cynch/DownloadType.java
@@ -5,6 +5,23 @@ package com.muddyhorse.cynch;
 
 public enum DownloadType {
     critical("Critical"), required("Required"), optional("Optional"), all("All");
+    public static DownloadType getTypeFromChar(char t) {
+        DownloadType type;
+
+        switch (t) {
+            case Constants.TYPE_CHAR_CRIT: // '!'
+                type = DownloadType.critical;
+            break;
+            case Constants.TYPE_CHAR_OPT: // '?'
+                type = DownloadType.optional;
+            break;
+            case Constants.TYPE_CHAR_REQUIRED: // '.'
+            default:                       // '.'
+                type = DownloadType.required;
+        } // endswitch
+        
+        return type;
+    }
     private final String description;
     private DownloadType(String description) {
         this.description = description;
